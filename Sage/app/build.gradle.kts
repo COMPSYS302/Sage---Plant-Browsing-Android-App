f896454a23d91885d3eeac81097f3168aab939e2
tche867 <160444495+TonnyChenProgramming@users.noreply.github.com>
2025-05-15 16:58:19 +1200

    sage startup

--- Sage/app/build.gradle.kts
+++ Sage/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.sage"
-        minSdk = 16
+        minSdk = 19
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
