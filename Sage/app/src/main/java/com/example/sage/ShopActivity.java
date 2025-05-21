e490cbbcfa17a25ad2c9ca12cb123b9f2465f71b
Zcol623 <zcol623@aucklanduni.ac.nz>
2025-05-21 14:31:04 +1200

    Bottom nav bar added and app icon

---
+++ Sage/app/src/main/java/com/example/sage/ShopActivity.java
@@ -0,0 +1,48 @@
+package com.example.sage;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_shop);
+        
+
+
+        // Linking bottom navigation to views
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_shop);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.bottom_shop) {
+                return true;
+            } else if (itemId == R.id.bottom_home) {
+                startActivity(new Intent(ShopActivity.this, MainActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (itemId == R.id.bottom_favourites) {
+                startActivity(new Intent(ShopActivity.this, FavouritesActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            }
+            return false;
+        });
+
+    }
+}
