b99f36cf155693344afd055180483b0092a6613a
DavidLi <dy11222@outloo.com>
2025-06-07 14:37:13 +1200

    Icons add to profile_menu.xml file, but it doesn't show up in the app

--- Sage/app/src/main/java/com/example/sage/LoginActivity.java
+++ Sage/app/src/main/java/com/example/sage/LoginActivity.java
@@ -5,6 +5,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
